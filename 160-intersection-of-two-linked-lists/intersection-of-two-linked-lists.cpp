8e98a77739432065863aa41e9530e77126f23998
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2024-06-15 06:57:02 +0530

    Time: 45 ms (27.06%) | Memory: 17.2 MB (35.04%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,77 +8,20 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-    //   while(headB!=NULL){
-    //     ListNode* temp = headA;
-    //     while(temp){
-    //         if(temp==headB){
-    //             return headB;
-    //         }
-    //         else{
-    //             temp=temp->next;
-    //         }
-    //     }
-    //     headB=headB->next;
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+    if (headA == NULL || headB == NULL) {
+        return NULL;
+    }
     
-    //   }
-    //   return NULL;
-
-// //2nd approach Hashing
-//     unordered_set<ListNode*> st;
-//     ListNode* temp=headA;
-//     while(headA){
-//         st.insert(headA);
-//         headA=headA->next;
-//     }
-//     while(headB){
-//         if(st.find(headB)!=st.end()){
-//             return headB;
-//         }
-//         headB=headB->next;
-//     }
-//     return NULL;
-
-int countA = 0;
-int countB = 0 ;
-ListNode* tempA = headA;
-ListNode* tempB = headB;
-while(tempA){
-    countA++;
-    tempA=tempA->next;
-}
-while(tempB){
-    countB++;
-    tempB=tempB->next;
-}
-int diff= abs(countA-countB);
-tempA=headA;
-tempB=headB;
-    if(countA>countB){
-    while(diff>0){
-     
-    tempA=tempA->next;
-       diff--;
+    ListNode* tempA = headA;
+    ListNode* tempB = headB;
+    
+    while (tempA != tempB) {
+        // Move to the next node or switch to the other list
+        tempA = (tempA == NULL) ? headB : tempA->next;
+        tempB = (tempB == NULL) ? headA : tempB->next;
     }
-    }else{
-        while(diff>0){
-           
-            tempB=tempB->next;
-             diff--;
-        }
-    }
-
-while (tempA && tempB && tempA != tempB) {
-    tempA = tempA->next;
-    tempB = tempB->next;
-}
-
-// If tempA and tempB are equal, it means there's an intersection
-if (tempA == tempB) {
+    
     return tempA;
 }
-
-return NULL;
-
-    }
 };
