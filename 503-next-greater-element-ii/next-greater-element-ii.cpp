d7fd398873e6a700f2f9b96729f53aaae1fdaa7a
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2024-09-01 12:20:06 +0530

    Time: 20 ms (72.07%) | Memory: 26.7 MB (83.72%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,17 +1,40 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        int siz = nums.size();
-        vector<int> nge(siz , -1);
-        for(int i = 0 ; i< siz ; i++){
-            for(int j=1 ;j<siz ; j++ ){
-                int index = (i+j)%siz;
-                if(nums[index]>nums[i]){
-                    nge[i]=nums[index];
-                    break;
+       
+       stack<int> st;
+       int n = nums.size();
+       vector<int> nge(n,-1);
+       for(int i = 2*n-1 ; i>=0 ; i--){
+            while(!st.empty() && st.top()<=nums[i%n]){
+                st.pop();
+            }
+            if(i<n){
+                if(!st.empty()){
+                    nge[i]=st.top();
+                }else{
+                   nge[i]=-1; 
                 }
             }
-        }
-        return nge;
+            st.push(nums[i%n]);
+       }
+            return nge;
     }
 };
+// class Solution {
+// public:
+//     vector<int> nextGreaterElements(vector<int>& nums) {
+//         int size = nums.size();
+//         vector<int> nge(size , -1);
+//         for(int i = 0 ; i<size ; i++){
+//             for(int j = 1 ; j< size ; j++){
+//                 int index= (i+j)%size;
+//                 if(nums[index]>nums[i]){
+//                     nge[i]=nums[index];
+//                     break;
+//                 }
+//             }
+//         }
+//         return nge;
+//     }
+// };
