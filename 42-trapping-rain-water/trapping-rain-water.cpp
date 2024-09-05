2a6081d19bb1e612847d3f0ba8b913a90e90155c
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2024-09-05 18:23:50 +0530

    Time: 7 ms (91.29%) | Memory: 24.8 MB (33.87%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,22 +2,53 @@ class Solution {
 public:
    int trap(vector<int>& height) {
         int n = height.size();
-        int prefix[n] , suffix[n];
-        prefix[0] = height[0];
-        for(int i = 1 ; i<n ; i++){
-            prefix[i]=max(prefix[i-1],height[i]);
+        int left = 0 , right = n-1 ;
+        int rws = 0;
+        int leftMax=0 , rightMax=0 ;
+        while(left<=right){
+            if(height[left]<=height[right]){
+                if(height[left]>=leftMax){
+                    leftMax = height[left];
+                }else{
+                    rws +=  leftMax - height[left];
+                }
+                left++;
+            }else{
+                if(height[right]>=rightMax){
+                    rightMax = height[right];
+                }else{
+                    rws += rightMax - height[right];
+                }
+                right--;
+            }
         }
-        suffix[n-1]=height[n-1];
-        for(int i = n-2 ; i>=0 ; i--){
-            suffix[i]=max(suffix[i+1] , height[i]);
-        }
-        int rainWaterTrapped = 0;
-        for(int i = 0 ; i<n ; i++){
-            rainWaterTrapped += min(prefix[i] , suffix[i]) - height[i];
-        }
-        return rainWaterTrapped;
+        return rws;
+        
     }
 };
+
+//optimal approach 1 
+
+// class Solution {
+// public:
+//    int trap(vector<int>& height) {
+//         int n = height.size();
+//         int prefix[n] , suffix[n];
+//         prefix[0] = height[0];
+//         for(int i = 1 ; i<n ; i++){
+//             prefix[i]=max(prefix[i-1],height[i]);
+//         }
+//         suffix[n-1]=height[n-1];
+//         for(int i = n-2 ; i>=0 ; i--){
+//             suffix[i]=max(suffix[i+1] , height[i]);
+//         }
+//         int rainWaterTrapped = 0;
+//         for(int i = 0 ; i<n ; i++){
+//             rainWaterTrapped += min(prefix[i] , suffix[i]) - height[i];
+//         }
+//         return rainWaterTrapped;
+//     }
+// };
 
 
 // Brute Approach
