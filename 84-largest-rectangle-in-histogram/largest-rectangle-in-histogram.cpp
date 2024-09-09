9082f305a240e293f1df52636499ceb7db79cc22
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2024-09-09 13:41:57 +0530

    Time: 89 ms (95.85%) | Memory: 78.1 MB (95.89%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -18,39 +18,61 @@
 
 // Optimal Approach
 
+// class Solution {
+// public:
+//     int largestRectangleArea(vector<int>& heights) {
+//         int n = heights.size();
+
+//         // We have to find PSE and NSE 
+//         stack<int> st;
+//         vector<int> leftS(n);
+//         vector<int>rightS(n);
+//         // NSE
+//         for(int i = 0 ; i<n ; i++){
+//             while(!st.empty() && heights[st.top()]>=heights[i]){
+//                 st.pop();
+//             }
+//             leftS[i] = st.empty()?0 : st.top()+1;
+//             st.push(i);
+//         } 
+//         // clearing stack for next use;
+//         while(!st.empty()){
+//             st.pop();
+//         }   
+//         //PSE
+//         for(int i = n-1 ; i>=0 ; i--){
+//             while(!st.empty() && heights[st.top()]>=heights[i]){
+//                 st.pop();
+//             }
+//             rightS[i]=st.empty() ? n-1 : st.top()-1;
+//             st.push(i);
+//         }  
+//         int maxA=0;
+//         for(int i = 0 ; i< n ; i++){
+//             maxA= max(maxA , heights[i] * (rightS[i]-leftS[i]+1));
+//         } 
+//         return maxA;
+//     }
+// };
+
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-
-        // We have to find PSE and NSE 
         stack<int> st;
-        vector<int> leftS(n);
-        vector<int>rightS(n);
-        // NSE
-        for(int i = 0 ; i<n ; i++){
-            while(!st.empty() && heights[st.top()]>=heights[i]){
-                st.pop();
-            }
-            leftS[i] = st.empty()?0 : st.top()+1;
-            st.push(i);
-        } 
-        // clearing stack for next use;
-        while(!st.empty()){
-            st.pop();
-        }   
-        //PSE
-        for(int i = n-1 ; i>=0 ; i--){
-            while(!st.empty() && heights[st.top()]>=heights[i]){
-                st.pop();
-            }
-            rightS[i]=st.empty() ? n-1 : st.top()-1;
-            st.push(i);
-        }  
+
         int maxA=0;
-        for(int i = 0 ; i< n ; i++){
-            maxA= max(maxA , heights[i] * (rightS[i]-leftS[i]+1));
-        } 
+        for(int i=0 ; i<=n ; i++){
+            while(!st.empty() && (i==n || heights[st.top()]>=heights[i])){
+                int height = heights[st.top()];
+                st.pop();
+                int width;
+                width = st.empty() ? i : i-  st.top() -1;
+                maxA = max(maxA , width * height );
+                
+            }
+            st.push(i);
+        }
         return maxA;
     }
 };
