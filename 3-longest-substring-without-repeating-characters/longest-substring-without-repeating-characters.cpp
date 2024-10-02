ed0178e6e66d1d8a9de6b2f77a0d73bec62136ed
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2024-10-02 19:52:12 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (79.88%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,25 +1,43 @@
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int maxans = 0;
+//         int size = s.size();
+//         if(size==0){
+//             return 0;
+//         }
+//         for(int i = 0 ; i<size ; i++){
+//             unordered_set<char> set;
+//             for(int j = i ; j<size ; j++){
+//                 if(set.find(s[j]) !=set.end()){
+//                     maxans = max(maxans  , j-i);
+//                     break;
+//                 }
+//                 set.insert(s[j]);
+//             }
+//             maxans = max(maxans , (int)set.size());
+//         }
+//         return maxans;
+//     }
+// };
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxans = 0;
+        vector<int> mpp(256,-1);
+        int left =0 , right = 0 ;
+        int length = 0;
         int size = s.size();
-        if(size==0){
-            return 0;
-        }
-        // if(s==" " || size==1){
-        //     return 1;
-        // }
-        for(int i = 0 ; i<size ; i++){
-            unordered_set<char> set;
-            for(int j = i ; j<size ; j++){
-                if(set.find(s[j]) !=set.end()){
-                    maxans = max(maxans  , j-i);
-                    break;
-                }
-                set.insert(s[j]);
+        while(right<size){
+            if(mpp[s[right]] != -1){
+                left = max(mpp[s[right]]+1 , left);
             }
-            maxans = max(maxans , (int)set.size());
+                mpp[s[right]]=right;
+                length=max(length , right-left+1);
+                right++;
+            
+           
         }
-        return maxans;
+         return length;
     }
 };
