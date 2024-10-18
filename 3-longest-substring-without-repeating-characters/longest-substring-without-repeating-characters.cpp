c266b8d5923f61a05f5625fe10f11c8f7eaebae8
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2024-10-18 19:31:36 +0530

    Time: 667 ms (5.00%) | Memory: 247.9 MB (5.03%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,43 +1,47 @@
-// class Solution {
-// public:
-//     int lengthOfLongestSubstring(string s) {
-//         int maxans = 0;
-//         int size = s.size();
-//         if(size==0){
-//             return 0;
-//         }
-//         for(int i = 0 ; i<size ; i++){
-//             unordered_set<char> set;
-//             for(int j = i ; j<size ; j++){
-//                 if(set.find(s[j]) !=set.end()){
-//                     maxans = max(maxans  , j-i);
-//                     break;
-//                 }
-//                 set.insert(s[j]);
-//             }
-//             maxans = max(maxans , (int)set.size());
-//         }
-//         return maxans;
-//     }
-// };
-
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> mpp(256,-1);
-        int left =0 , right = 0 ;
-        int length = 0;
+        int maxans = 0 ;
         int size = s.size();
-        while(right<size){
-            if(mpp[s[right]] != -1){
-                left = max(mpp[s[right]]+1 , left);
-            }
-                mpp[s[right]]=right;
-                length=max(length , right-left+1);
-                right++;
-            
-           
+        if(size==0){
+            return 0;
         }
-         return length;
+        if(size==1){
+            return 1;
+        }
+        for(int i = 0 ; i<size ; i++){
+            unordered_set<char> set;
+            for(int j = i ; j<size ; j++){
+                if(set.find(s[j]) != set.end()){
+                    maxans = max(maxans , j-i);
+                    break;
+                }
+                set.insert(s[j]);
+            }
+            maxans = max(maxans , (int)set.size());
+
+        }
+        return maxans;
     }
 };
+
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         vector<int> mpp(256,-1);
+//         int left =0 , right = 0 ;
+//         int length = 0;
+//         int size = s.size();
+//         while(right<size){
+//             if(mpp[s[right]] != -1){
+//                 left = max(mpp[s[right]]+1 , left);
+//             }
+//                 mpp[s[right]]=right;
+//                 length=max(length , right-left+1);
+//                 right++;
+            
+           
+//         }
+//          return length;
+//     }
+// };
