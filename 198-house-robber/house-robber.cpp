d5a5e918b635f9750c280cad07f08f10921292c8
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2024-11-03 23:53:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (86.08%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -22,24 +22,50 @@
 //     }
 // };
 
+// class Solution {
+// public:
+// int function(int ind , vector<int> &nums , vector<int> &dp){
+//     int n = nums.size();
+//     int prev = nums[0];
+//     int prev2 = 0;
+
+//     for(int i = 1; i<n ; i++){
+//         int take = nums[i];
+//         if(i>1) take+=prev2;
+
+//         int notTake = 0 + prev;
+
+//         int curi = max(take , notTake);
+//         prev2 = prev;
+//         prev=curi;
+//     }
+//     return prev;
+// }
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> dp(n , -1);
+//         return function(n -1, nums , dp);
+        
+//     }
+// };
+
 class Solution {
 public:
 int function(int ind , vector<int> &nums , vector<int> &dp){
     int n = nums.size();
-    int prev = nums[0];
-    int prev2 = 0;
+     dp[0] = nums[0];
 
     for(int i = 1; i<n ; i++){
         int take = nums[i];
-        if(i>1) take+=prev2;
+        if(i>1){
+             take = take + dp[i-2];
+        }
+        int notTake = 0 + dp[i-1];
 
-        int notTake = 0 + prev;
-
-        int curi = max(take , notTake);
-        prev2 = prev;
-        prev=curi;
+        dp[i] = max(take , notTake);
+         
     }
-    return prev;
+    return dp[n-1];
 }
     int rob(vector<int>& nums) {
         int n = nums.size();
