2a5ac036a5bd7a66d2ac3b2c1d9cf2b0e4fcb734
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2024-11-03 23:45:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (45.12%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,18 +1,45 @@
+// class Solution {
+// public:
+// int function(int ind , vector<int> &nums , vector<int> &dp){
+//     if(ind==0){
+//         return nums[ind];
+//     }
+//     if(ind<0){
+//         return 0;
+//     }
+//     if (dp[ind]!= -1) return dp[ind];
+
+//     int pick = nums[ind]+function(ind-2 , nums , dp);
+//     int notpick = 0 + function(ind-1 , nums , dp);
+//     return dp[ind] = max(pick , notpick);
+
+// }
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> dp(n , -1);
+//         return function(n -1, nums , dp);
+        
+//     }
+// };
+
 class Solution {
 public:
 int function(int ind , vector<int> &nums , vector<int> &dp){
-    if(ind==0){
-        return nums[ind];
-    }
-    if(ind<0){
-        return 0;
-    }
-    if (dp[ind]!= -1) return dp[ind];
+    int n = nums.size();
+    int prev = nums[0];
+    int prev2 = 0;
 
-    int pick = nums[ind]+function(ind-2 , nums , dp);
-    int notpick = 0 + function(ind-1 , nums , dp);
-    return dp[ind] = max(pick , notpick);
+    for(int i = 1; i<n ; i++){
+        int take = nums[i];
+        if(i>1) take+=prev2;
 
+        int notTake = 0 + prev;
+
+        int curi = max(take , notTake);
+        prev2 = prev;
+        prev=curi;
+    }
+    return prev;
 }
     int rob(vector<int>& nums) {
         int n = nums.size();
