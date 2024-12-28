5cd4a7c2b4bddc3042869902b02688019b878006
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2024-12-28 23:44:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (62.73%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,20 +1,37 @@
+// class Solution {
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         int n=nums.size();
+//         int totalSubset=1<<n;
+//         vector<vector<int>> result;
+//         for(int i = 0 ; i<totalSubset ; i++){
+//             vector<int>subset;
+//             for(int j=0 ; j<n ; j++){
+//                 //check if the j-th bit in the binary representation
+//                 if(i&(1<<j)){
+//                     subset.push_back(nums[j]);
+//                 }
+//             }
+//             result.push_back(subset);
+//         }
+//         return result;
+//     }
+// };
+// using backtracking 
 class Solution {
 public:
+void backtrack(int start , vector<int>&nums , vector<int> &current , vector<vector<int>>&result){
+    result.push_back(current);
+    for(int i=start ; i<nums.size() ; i++){
+        current.push_back(nums[i]);
+        backtrack(i+1 , nums , current , result);
+        current.pop_back();
+    }
+}
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n=nums.size();
-        int totalSubset=1<<n;
-        vector<vector<int>> result;
-        for(int i = 0 ; i<totalSubset ; i++){
-            //check if the bit is set or not
-            vector<int>subset;
-            for(int j=0 ; j<n ; j++){
-                //check if the j-th bit in the binary representation
-                if(i&(1<<j)){
-                    subset.push_back(nums[j]);
-                }
-            }
-            result.push_back(subset);
-        }
+        vector<vector<int>>result;
+        vector<int> current;
+        backtrack(0,nums,current,result);
         return result;
     }
 };
