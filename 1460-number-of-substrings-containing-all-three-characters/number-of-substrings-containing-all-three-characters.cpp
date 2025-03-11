4ac194138938353d3e4c6aba9833ec3344eed5f9
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-03-11 23:20:42 +0530

    Time: 8 ms (61.55%) | Memory: 10.9 MB (94.17%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,38 +1,19 @@
-
-    
-
-
-
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        // int count = 0;
-        // int n=s.size();
-        // for(int i = 0 ; i<s.size() ; i++){
-        //     int hash[3]={0};
-        //     for(int j=i ; j<s.size() ; j++){
-        //         hash[s[j]-'a']=1;
-        //         if(hash[0]+hash[1]+hash[2]==3){
-        //             count = count + (n-j);
-        //             break;
-        //         }
-        //     }
-        // }
-        // return count;
-
-        //BRUTE FORCE APPROACH
-        
         int count = 0;
-        int n=s.size();
-        int lastSeen[3]={-1,-1,-1};
-        for(int i = 0 ;i<n ; i++){
-             lastSeen[s[i]-'a']=i;
-            if(lastSeen[0]!=-1 && lastSeen[1]!=-1 && lastSeen[2]!=-1){
-               int minLastSeen =  min({lastSeen[0],lastSeen[1],lastSeen[2]});
-               count += (1+minLastSeen);
+        int n = s.size();
+        int lastSeen[3] = {-1, -1, -1}; // Last seen indices for 'a', 'b', and 'c'
+
+        for (int i = 0; i < n; i++) {
+            lastSeen[s[i] - 'a'] = i; // Update last seen index for current character
+
+            // If all characters have been seen at least once
+            if (lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1) {
+                int minLastSeen = min({lastSeen[0], lastSeen[1], lastSeen[2]});
+                count += (1 + minLastSeen);
             }
         }
         return count;
-        
     }
 };
