abff130d0d12f9ba29bd02df708307ab124c5660
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-03-26 03:26:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (34.30%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> triangle(numRows);
-        for(int i = 0 ; i< numRows ; i++){
-            triangle[i].resize(i+1);
-                triangle[i][0]=triangle[i][i]=1;
-                for(int j= 1 ; j< i ; j++){
-                    triangle[i][j] = triangle[i-1][j-1]+triangle[i-1][j];
-                }
+    vector<int>generateRows(int ind){
+        int sum=1;
+        vector<int>row;
+        row.push_back(sum);
+        for(int i = 1 ; i<ind ; i++){
+            sum=sum*(ind-i);
+            sum = sum/i;
+            row.push_back(sum);
+
         }
-        return triangle;
+        return row;
+    }
+
+    vector<vector<int>> generate(int numRows) {
+        
+        vector<vector<int>>ans;
+        for(int i = 1 ; i<=numRows ; i++){
+            ans.push_back(generateRows(i));
+        }
+return ans;
     }
 };
