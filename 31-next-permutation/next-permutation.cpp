e46cd5682fd191fb7a0c25265d70e3d0b16894c8
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-03-26 04:41:22 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (32.91%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int breakpoint=-1;
+        for(int i=n-2 ; i>=0 ; i--){
+            if(nums[i+1]>nums[i]){
+                breakpoint=i;
+                break;
+            }
+        }
+        if(breakpoint==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        for(int i = n-1; i>breakpoint ; i--){
+            if(nums[breakpoint]<nums[i]){
+                swap(nums[i],nums[breakpoint]);
+                break;
+            }
+        }
+        reverse(nums.begin()+breakpoint+1 , nums.end());
+    }
+};
