f2230b4d3157d9586e99d9e82279a172c6ccac3c
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-05-01 00:33:30 +0530

    Time: 12 ms (44.49%) | Memory: 67.6 MB (21.89%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,19 +1,34 @@
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         int ans=0;
+//         int n=nums.size();
+//         sort(nums.begin() , nums.end());
+//         int i=0 ;
+//         while(i<=n){
+//             if(nums[i]==nums[i+1]){
+//                 ans=nums[i];
+//                 break;
+//             }else{
+//                  i++;
+//         }
+//         }
+        
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int ans=0;
-        int n=nums.size();
-        sort(nums.begin() , nums.end());
-        int i=0 ;
-        while(i<=n){
-            if(nums[i]==nums[i+1]){
-                ans=nums[i];
-                break;
-            }else{
-                 i++;
+        int n = nums.size();
+       vector<int> freq(n,0);
+       for(int i=0 ; i<n ; i++){
+        if(freq[nums[i]]==0){
+            freq[nums[i]]+=1;
+        }else{
+            return nums[i];
         }
-        }
-        
-        return ans;
+       }
+       return 0;
     }
 };
