2f0b7273549cddc81996711be86105804c130eac
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-05-19 12:13:51 +0530

    Time: 3 ms (34.60%) | Memory: 77.3 MB (12.30%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -10,46 +10,28 @@
  */
 class Solution {
 public:
-    // ListNode* reverse(ListNode* head){
-    //     ListNode* temp = head;
-    //     ListNode* prev= head;
-    //     while(temp){
-    //         ListNode* front = temp->next;
-    //         temp->next= prev;
-    //         prev=temp;
-    //         temp=front;
-    //     }
-    //     return prev;
-
-    // }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        // l1= reverse(l1);
-        // l2= reverse(l2);
-
-        ListNode* dummyNode = new ListNode(-1);
-        ListNode* curr = dummyNode;
-        int carry = 0 ;
-        while(l1 != NULL || l2 != NULL){
-            int x = (l1 != NULL) ? l1->val : 0 ;
-            int y = (l2 != NULL) ? l2->val : 0 ;
-            int sum = x+y+carry;
-
-            carry = sum/10;
-
-            curr->next = new ListNode(sum%10);
-            curr=curr->next;
-
-            if(l1 != NULL){
-                l1=l1->next;
-            }
-            if(l2!= NULL){
-                l2=l2->next;
-            }
+        ListNode* temp1 = l1;
+        ListNode* temp2 = l2;
+        int carry =0;
+        ListNode *dummy = new ListNode();
+        ListNode *temp=dummy;
+        while( (temp1!= NULL || temp2 != NULL) || carry) {
+            int sum=0;
+        if(temp1!=nullptr){
+            sum+=temp1->val;
+            temp1=temp1->next;
         }
-        if(carry>0){
-            curr->next = new ListNode(carry);
-            curr=curr->next;
+        if(temp2!=nullptr){
+            sum+=temp2->val;
+            temp2=temp2->next;
         }
-        return dummyNode->next;
+        sum +=carry;
+        carry=sum/10;
+        ListNode* node = new ListNode(sum%10);
+        temp->next=node;
+        temp=temp->next;
+        }
+        return dummy->next;
     }
 };
