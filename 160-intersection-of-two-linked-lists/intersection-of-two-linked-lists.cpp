43203c4a9360e95bd749d5796dbf6239a9618abc
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-05-19 13:06:22 +0530

    Time: 39 ms (43.84%) | Memory: 18.5 MB (53.67%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -1,3 +1,27 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode(int x) : val(x), next(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         while(headB!=nullptr){
+//             ListNode* temp = headA;
+//             while(temp!=nullptr){
+//                 if(temp == headB){
+//                     return headB;
+//                 }
+//                 temp=temp->next;
+//             }
+//             headB=headB->next;
+//         }
+//         return NULL;
+//     }
+// };
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -9,15 +33,29 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        while(headB!=nullptr){
-            ListNode* temp = headA;
-            while(temp!=nullptr){
-                if(temp == headB){
-                    return headB;
-                }
-                temp=temp->next;
-            }
+        int l1=0 , l2=0 ;
+        ListNode* dummy1 = headA;
+        ListNode* dummy2 = headB;
+        while(dummy1 != nullptr){
+            l1++;
+            dummy1=dummy1->next;
+        }
+        while(dummy2 != nullptr){
+            l2++;
+            dummy2=dummy2->next;
+        }
+        int len = abs( l1-l2);
+        dummy1=headA;
+        dummy2=headB;
+        if(l1>l2){
+            while(len--) headA=headA->next;
+        }else {
+           while(len--) headB=headB->next;
+        }
+        while(headA!=nullptr){
+            if(headA==headB) return headA;
             headB=headB->next;
+            headA=headA->next;
         }
         return NULL;
     }
