5391300b50583d80e7dc230497c5915395d10f78
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-05-19 14:01:51 +0530

    Time: 8 ms (81.37%) | Memory: 11.8 MB (52.80%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -12,17 +12,25 @@ public:
         if(head==nullptr || head->next==nullptr){
             return false;
         }
-        unordered_set<ListNode*> st;
-        ListNode* temp= head;
-        while(temp!=nullptr){
-            if(st.find(temp) !=st.end()){
-               return true;
-            }
-            st.insert(temp);
-            temp=temp->next;
+    //     unordered_set<ListNode*> st;
+    //     ListNode* temp= head;
+    //     while(temp!=nullptr){
+    //         if(st.find(temp) !=st.end()){
+    //            return true;
+    //         }
+    //         st.insert(temp);
+    //         temp=temp->next;
             
-        }
+    //     }
 
+    // return false;
+    ListNode* slow=head;
+    ListNode* fast = head;
+    while(fast!=nullptr && fast->next !=nullptr ){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast) return true;
+    }
     return false;
     }
 };
