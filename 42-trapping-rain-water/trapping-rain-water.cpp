35c9ddacc2af2ed635d93c9755729ec115d46d4a
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-05-23 10:28:35 +0530

    Time: 0 ms (100.00%) | Memory: 27.5 MB (8.32%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,78 +1,42 @@
 class Solution {
 public:
-   int trap(vector<int>& height) {
+    int trap(vector<int>& height) {
+        //BETTER APPROACH  STORE PREFIX AND SUFFIX 
+        int waterTrapped = 0 ;
         int n = height.size();
-        int left = 0 , right = n-1 ;
-        int rws = 0;
-        int leftMax=0 , rightMax=0 ;
-        while(left<=right){
-            if(height[left]<=height[right]){
-                if(height[left]>=leftMax){
-                    leftMax = height[left];
-                }else{
-                    rws +=  leftMax - height[left];
-                }
-                left++;
-            }else{
-                if(height[right]>=rightMax){
-                    rightMax = height[right];
-                }else{
-                    rws += rightMax - height[right];
-                }
-                right--;
-            }
+        vector<int>prefix(n) ; vector<int>suffix(n);
+        prefix[0]=height[0];
+        for(int i=1 ; i<n ; i++){
+            prefix[i]=max(prefix[i-1] , height[i]);
         }
-        return rws;
-        
+        suffix[n-1]=height[n-1];
+        for(int i=n-2 ; i>=0 ; i--){
+            suffix[i]=max(suffix[i+1] , height[i]);
+        }
+        for(int i=0 ; i<n ; i++){
+            waterTrapped+=min(prefix[i] , suffix[i])-height[i];
+        }
+        return waterTrapped;
+
+
+        // // BRUTE APPROACH
+
+        // int waterTrapped = 0 ;
+        // int n=height.size();
+        // for(int i=0 ; i<n ; i++){
+        //     int leftMax=0 , rightMax=0;
+        //     int j=i;
+        //     while(j>=0){
+        //         leftMax= max(leftMax , height[j]);
+        //         j--;
+        //     }
+        //     j=i;
+        //     while(j<n){
+        //         rightMax=max(rightMax , height[j]);
+        //         j++;
+        //     }
+        //     waterTrapped += min(leftMax , rightMax)-height[i];
+        // }
+        // return waterTrapped;
     }
 };
-
-//optimal approach 1 
-
-// class Solution {
-// public:
-//    int trap(vector<int>& height) {
-//         int n = height.size();
-//         int prefix[n] , suffix[n];
-//         prefix[0] = height[0];
-//         for(int i = 1 ; i<n ; i++){
-//             prefix[i]=max(prefix[i-1],height[i]);
-//         }
-//         suffix[n-1]=height[n-1];
-//         for(int i = n-2 ; i>=0 ; i--){
-//             suffix[i]=max(suffix[i+1] , height[i]);
-//         }
-//         int rainWaterTrapped = 0;
-//         for(int i = 0 ; i<n ; i++){
-//             rainWaterTrapped += min(prefix[i] , suffix[i]) - height[i];
-//         }
-//         return rainWaterTrapped;
-//     }
-// };
-
-
-// Brute Approach
-
-// class Solution {
-// public:
-//     int trap(vector<int>& height) {
-//         int size = height.size();
-//         int rainWaterTrapped = 0;
-//         for(int i = 0 ; i<size ; i++){
-//             int j = i;
-//             int leftMax = 0 , rightMax = 0 ;
-//             while(j>=0 ){
-//                 leftMax = max(leftMax , height[j]);
-//                 j--;
-//             }
-//             j=i;
-//             while(j<size){
-//                 rightMax = max(rightMax , height[j]);
-//                 j++;
-//             }
-//             rainWaterTrapped += min(leftMax , rightMax)- height[i];
-//         }
-//         return  rainWaterTrapped;
-
-//     }
-// };
