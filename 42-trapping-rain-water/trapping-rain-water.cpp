ee77f4563e2a23ec18aeaed4b67a571a60e07801
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-05-23 11:07:19 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (79.92%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,22 +1,48 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        //BETTER APPROACH  STORE PREFIX AND SUFFIX 
-        int waterTrapped = 0 ;
-        int n = height.size();
-        vector<int>prefix(n) ; vector<int>suffix(n);
-        prefix[0]=height[0];
-        for(int i=1 ; i<n ; i++){
-            prefix[i]=max(prefix[i-1] , height[i]);
-        }
-        suffix[n-1]=height[n-1];
-        for(int i=n-2 ; i>=0 ; i--){
-            suffix[i]=max(suffix[i+1] , height[i]);
-        }
-        for(int i=0 ; i<n ; i++){
-            waterTrapped+=min(prefix[i] , suffix[i])-height[i];
+
+        //OPTIMAL APPROACH
+        int n=height.size();
+        int left = 0 , right = n-1 ;
+        int leftMax=0 , rightMax=0;
+        int waterTrapped=0;
+        while(left<=right){
+            if(height[left]<=height[right]){
+                if(height[left]>=leftMax){
+                    leftMax=height[left];
+                }else{
+                    waterTrapped += leftMax-height[left];
+                }
+                left++;
+            }else{
+                if(height[right]>=rightMax){
+                    rightMax=height[right];
+                }else{
+                    waterTrapped+=rightMax-height[right];
+                } 
+                right--;
+            }
         }
         return waterTrapped;
+
+
+        // //BETTER APPROACH  STORE PREFIX AND SUFFIX 
+        // int waterTrapped = 0 ;
+        // int n = height.size();
+        // vector<int>prefix(n) ; vector<int>suffix(n);
+        // prefix[0]=height[0];
+        // for(int i=1 ; i<n ; i++){
+        //     prefix[i]=max(prefix[i-1] , height[i]);
+        // }
+        // suffix[n-1]=height[n-1];
+        // for(int i=n-2 ; i>=0 ; i--){
+        //     suffix[i]=max(suffix[i+1] , height[i]);
+        // }
+        // for(int i=0 ; i<n ; i++){
+        //     waterTrapped+=min(prefix[i] , suffix[i])-height[i];
+        // }
+        // return waterTrapped;
 
 
         // // BRUTE APPROACH
