bb2ebc042e64e7f61527fb139b81653dded46bab
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-05-28 23:52:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (99.48%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int length =0 ;
-        int i = s.length()-1;
-        while(i>=0 && s[i]==' '){
+        int n = s.size();
+        int length = 0;
+        int i = n - 1;
+
+        // Step 1: Skip trailing spaces
+        while (i >= 0 && s[i] == ' ') {
             i--;
         }
-        while(i>=0 && s[i]!=' '){
+
+        // Step 2: Count characters of the last word
+        while (i >= 0 && s[i] != ' ') {
             length++;
             i--;
         }
-    return length;
+
+        return length;
     }
 };
