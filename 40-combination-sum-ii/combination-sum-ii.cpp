1145b26cf7c44afe16aee51ea165fe4067781d17
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-06-04 00:26:58 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (38.93%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    void findCombination(int ind , vector<int>&candidates ,int target, vector<vector<int>> &result , vector<int>&ans){
-            if(target==0){
+     void findC(int ind  , vector<int>&candidates , int target , vector<int>&ans , vector<vector<int>>&result){
+        if(target==0){
             result.push_back(ans);
-            return;
+            return ;
         }
-       for(int i = ind ; i<candidates.size() ; i++){
-        if(i>ind && candidates[i]==candidates[i-1]){
-            continue;
+        int n=candidates.size();
+        for(int i=ind ; i<n ; i++){
+            if(i>ind && candidates[i]==candidates[i-1]) continue;
+            if(candidates[i]>target) break;
+            ans.push_back(candidates[i]);
+            findC(i+1 , candidates , target-candidates[i], ans , result);
+            ans.pop_back();
         }
-        if(candidates[i]>target){
-            break;
-        }
-        ans.push_back(candidates[i]);
-        findCombination(i+1,candidates,target-candidates[i],result,ans);
-        ans.pop_back();
-       }
-        
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>>result;
-        vector<int>ans;
         sort(candidates.begin() , candidates.end());
-        findCombination(0,candidates,target,result,ans);
+        vector<int> ans;
+        vector<vector<int>> result;
+        findC(0 , candidates , target , ans , result);
         return result;
     }
 };
