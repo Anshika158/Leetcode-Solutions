1ee9f719e39a290156d50ecf1f73b5c3defb2f50
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-06-04 01:01:13 +0530

    Time: 58 ms (46.13%) | Memory: 61.8 MB (37.20%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,44 +1,31 @@
 class Solution {
 public:
+void findC(int ind , string s , vector<vector<string>>&res , vector<string>&path){
+    int n=s.size();
+    if(ind==n){
+        res.push_back(path);
+        return;
+    }
+    for(int i=ind ; i<n ; i++){
+        if(isPalindrome(s,ind,i)){
+            path.push_back(s.substr(ind,i-ind+1));
+            findC(i+1,s,res,path);
+            path.pop_back();
+        }
+    }
+}
+bool isPalindrome(string s , int start , int end){
+    while(start<=end){
+        if(s[start++]!=s[end--]){
+            return false;
+        }
+    }
+    return true;
+}
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> result; // To store all the partitions
-        vector<string> currentPartition; // To store the current partition
-        backtrack(0, s, currentPartition, result); // Start backtracking
-        return result;
-    }
-
-private:
-    // Helper function to check if a substring is a palindrome
-    bool isPalindrome(const string& s, int start, int end) {
-        while (start < end) {
-            if (s[start] != s[end]) return false; // Not a palindrome
-            start++;
-            end--;
-        }
-        return true; // Is a palindrome
-    }
-
-    // Backtracking function
-    void backtrack(int start, const string& s, vector<string>& currentPartition, vector<vector<string>>& result) {
-        // Base case: If start index reaches the end of the string
-        if (start == s.size()) {
-            result.push_back(currentPartition); // Add the current partition to the result
-            return;
-        }
-
-        // Explore all possible substrings starting from 'start'
-        for (int end = start; end < s.size(); end++) {
-            // Check if the substring s[start:end+1] is a palindrome
-            if (isPalindrome(s, start, end)) {
-                // Add the palindrome substring to the current partition
-                currentPartition.push_back(s.substr(start, end - start + 1));
-
-                // Recur for the remaining substring
-                backtrack(end + 1, s, currentPartition, result);
-
-                // Backtrack: Remove the last added substring
-                currentPartition.pop_back();
-            }
-        }
+        vector<vector<string>> res;
+        vector<string>path;
+        findC(0,s,res,path);
+        return res;
     }
 };
