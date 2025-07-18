62bdde1c03e9a3e5c4d15fd8274df5b5e1651a6d
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-07-19 01:34:23 +0530

    Time: 0 ms (100.00%) | Memory: 59.3 MB (25.67%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -2,23 +2,20 @@
 // class Solution {
 // public:
 //     int longestOnes(vector<int>& nums, int k) {
-//        int maxLength = 0 ; 
+//         int maxLength = 0 ;
 //        int length = 0 ;
-//        int zeroCount=0;
-//        int size = nums.size();
-//        for(int i = 0 ; i < size ; i++){
-//         for(int j = i ; j<size ; j++){
+//        for(int i=0 ; i<nums.size() ; i++){
+//         int zeroCount=0;
+//         for(int j=i ;j<nums.size() ; j++){
 //             if(nums[j]==0){
 //                 zeroCount++;
 //             }
-//             if(zeroCount > k){
-//                 break;
+//             if(zeroCount<=k){
+//                 length = j-i+1;
+//                 maxLength= max(maxLength , length);
 //             }
-//             length = j-i+1;
 //         }
-//         maxLength = max(maxLength , length);
-//         zeroCount = 0;
-//        } 
+//        }
 //        return maxLength;
 //     }
 // };
@@ -26,27 +23,23 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int length = 0 , maxlength = 0 ;
-        int left = 0 , right = 0;
-        int zeroCount =0;
-        int size = nums.size();
-        while(right<size){
-            if(nums[right]==0){
-                zeroCount++;
-            }
-            while(zeroCount>k){
-                if(nums[left]==0){
-                    zeroCount--;
-                }
-                left++;
-            }
-            if(zeroCount<=k){
-                length = right-left+1;
-                maxlength = max(maxlength , length);
-            }
-            right++;
+      int length = 0 , maxLength = 0 , left = 0 , right =0 , zeroCount=0;
+
+      while(right<nums.size()){
+        if(nums[right]==0){
+            zeroCount++;
         }
-        return maxlength;
+        while(zeroCount>k){
+            if(nums[left]==0){
+                zeroCount--;
+            }
+            left++;
+        }
+        length = right-left+1;
+        maxLength=max(length , maxLength);
+        right++;
+      }
+      return maxLength;
     }
 };
 
@@ -54,26 +47,24 @@ public:
 // public:
 //     int longestOnes(vector<int>& nums, int k) {
 //         int length = 0, zeroCount=0 , maxLength=0;
-//         int n = nums.size();
+//         int size = nums.size();
 //         int left = 0 , right = 0 ;
-//         while(right<n){
+//         while(right<size){
 //             if(nums[right]==0){
 //                 zeroCount++;
 //             }
-//             if(zeroCount>k){
+//             while(zeroCount > k){
 //                 if(nums[left]==0){
 //                     zeroCount--;
-                    
 //                 }
 //                 left++;
+//             }
 //                 if(zeroCount<=k){
 //                     length = right-left+1;
 //                     maxLength = max(maxLength , length);
 //                 }
-
-//             }
-//             right++;
+//                 right++;
 //         }
-//            return maxLength; 
+//         return maxLength;
 //     }
 // };
