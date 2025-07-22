cb0e401cbe4984ea88abfb208c447a70575bbab3
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-07-23 02:41:09 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (94.49%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int count = 0;
-        int n = s.size();
-        int lastSeen[3] = {-1, -1, -1}; // Last seen indices for 'a', 'b', and 'c'
-
-        for (int i = 0; i < n; i++) {
-            lastSeen[s[i] - 'a'] = i; // Update last seen index for current character
-
-            // If all characters have been seen at least once
-            if (lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1) {
-                int minLastSeen = min({lastSeen[0], lastSeen[1], lastSeen[2]});
-                count += (1 + minLastSeen);
-            }
+      int count=0;
+      int lastSeen[3]={-1,-1 ,-1};
+      for(int i=0 ;i<s.size() ; i++){
+        lastSeen[s[i]-'a']=i;
+        if(lastSeen[0]!=-1 && lastSeen[1]!=-1 && lastSeen[2]!=-1){
+            
+            count+=1+min(min(lastSeen[0] , lastSeen[1] ), lastSeen[2]);
         }
-        return count;
+      }
+      return count;
     }
 };
