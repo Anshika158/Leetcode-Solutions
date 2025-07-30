92baa473d7f6c6c83a76c3edb8146a319a22dd13
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-07-30 17:42:12 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (49.11%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=1 ; i<strs.size() ; i++){
             string s1 = strs[i];
             int pointer=0;
-            while(pointer<s1.size()){
+            while(pointer<s1.size() && pointer<answer.size()){
                 if(s1[pointer]==answer[pointer]){
                     pointer++;
                 }else{
