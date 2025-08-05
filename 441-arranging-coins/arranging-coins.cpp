d6061f91b891a996f708e989aac463182a96f72d
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-08-05 11:47:47 +0530

    Time: 4 ms (30.35%) | Memory: 8.7 MB (68.50%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        if(n==0 ||n==1) return n;
+        int i=1;
+        int count=0;
+        while(i<=n){ //1<3   
+            if(n>=i){  // 3>1
+                n= n-i;   // 3-1 = 2    n==2 
+                count++; //count=1  
+            }
+            i++;  //i=2  
+        }
+        return count;
+    }
+};
