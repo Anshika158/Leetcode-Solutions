40d4ca7e65f39fa7c4223c43607c7730789193f7
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-09-17 13:17:41 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (92.42%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,17 +1,38 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        // int n=nums.size();
+        // vector<int>visited(n+1,0);
+        // for(int i=0 ;i< n ; i++){
+        //     int temp=nums[i];
+        //     visited[temp]=1;
+        // }
+        // for(int i=0 ; i<visited.size() ; i++){
+        //     if(visited[i]==0){
+        //         return i;
+        //     }
+        // }
+        // return 0;
+
+        //OPTIMAL APPROACH 1
+
+        // int n=nums.size();
+        // int sum= (n*(n+1))/2;
+        // int s=0;
+        // for(int i=0 ; i<n ; i++){
+        //     s+=nums[i];
+        // } 
+        // return sum-s;
+
+        // OPTIMAL APPROACH 2 USING XOR 
         int n=nums.size();
-        vector<int>visited(n+1,0);
-        for(int i=0 ;i< n ; i++){
-            int temp=nums[i];
-            visited[temp]=1;
+        int xorVal = 0;
+        for(auto num: nums){
+            xorVal ^= num;
         }
-        for(int i=0 ; i<visited.size() ; i++){
-            if(visited[i]==0){
-                return i;
-            }
+        for(int i=0 ; i<=n ; i++){
+            xorVal^=i;
         }
-        return 0;
+    return xorVal;
     }
 };
