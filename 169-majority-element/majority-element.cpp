99d79883e88fb719169a7717d91208735857286d
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-09-18 12:46:33 +0530

    Time: 0 ms (100.00%) | Memory: 28 MB (86.68%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // int candidate = 0 ;
-        // int count = 0 ;
-        // for(int num : nums){
-        //     if(count == 0){
-        //         candidate=num;
-        //     }
-        //     if(num==candidate){
-        //         count++;
-        //     }else{
-        //         count--;
-        //     }
-        // }
-        // int count1=0;
-        // for(int i=0 ; i<nums.size() ; i++){
-        //     if(nums[i]==candidate){
-        //         count1++;
-        //     }
-        // }
-        // if(count1 > (nums.size()/2)){
-        //     return candidate;
-        // }
-        // return -1;
-        
-        int n=nums.size();
-        int count = ceil(n/2.0);
-        map<int , int>mpp;
-        for(int i=0 ;i<n ;i++){
-            mpp[nums[i]]++;
-        }
-        for(auto it : mpp){
-            if(it.second>=count){
-                return it.first;
+        int candidate = 0 ;
+        int count = 0 ;
+        for(int num : nums){
+            if(count == 0){
+                candidate=num;
+            }
+            if(num==candidate){
+                count++;
+            }else{
+                count--;
             }
         }
-        return -1;
+        return candidate;
+        
+        // int n=nums.size();
+        // int count = ceil(n/2.0);
+        // map<int , int>mpp;
+        // for(int i=0 ;i<n ;i++){
+        //     mpp[nums[i]]++;
+        // }
+        // for(auto it : mpp){
+        //     if(it.second>=count){
+        //         return it.first;
+        //     }
+        // }
+        // return -1;
     }
 };
