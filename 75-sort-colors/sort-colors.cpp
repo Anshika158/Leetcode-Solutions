9ea1aa661f7b27684db5d0064c61c04d4a2a875f
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-09-18 11:45:40 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (49.06%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int zeroCount=0 , oneCount=0 , twoCount=0;
-        int n=nums.size();
-        for(int i=0 ; i<n ; i++){
-            if(nums[i]==0){
-                zeroCount++;
-            }else if(nums[i]==1){
-                oneCount++;
-            }else{
-                twoCount++;
+        // int zeroCount=0 , oneCount=0 , twoCount=0;
+        // int n=nums.size();
+        // for(int i=0 ; i<n ; i++){
+        //     if(nums[i]==0){
+        //         zeroCount++;
+        //     }else if(nums[i]==1){
+        //         oneCount++;
+        //     }else{
+        //         twoCount++;
+        //     }
+        // }
+        // for(int i=0 ; i<zeroCount ; i++)nums[i]=0;
+        // for(int i=zeroCount ; i<zeroCount+oneCount ; i++)nums[i]=1;
+        //  for(int i=oneCount+zeroCount ; i<n ; i++)nums[i]=2;
+
+        //OPTIMAL APPROACH USING LOW HIGH AND MID
+        int low=0 , mid =0 , high = nums.size()-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low] , nums[mid]);
+                low++;
+                mid++;
+            }else if(nums[mid]==2){
+                swap(nums[mid] , nums[high]);
+                high--;
+            }else if(nums[mid]==1){
+                mid++;
             }
         }
-        for(int i=0 ; i<zeroCount ; i++)nums[i]=0;
-        for(int i=zeroCount ; i<zeroCount+oneCount ; i++)nums[i]=1;
-         for(int i=oneCount+zeroCount ; i<n ; i++)nums[i]=2;
     }
 };
