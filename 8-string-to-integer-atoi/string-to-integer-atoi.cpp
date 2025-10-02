a21d962f2855d8be598d0cee6a80dfe694fd510f
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-10-03 01:50:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (55.46%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,34 +1,61 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        long long result=0;
-        int i=0 ;
-        int n =s.size();
-        int sign=1;
-        while(i<n && s[i]==' ' && s[i]!='-' && s[i] != '+' ){
-            i++;//i=5
-        }
-        if( (s[i]=='-' || s[i]=='+')){
-            sign= (s[i]=='-')?-1:1;//sign = -1
+        long long answer=0;
+        int n=s.size() , i=0;
+        int sign=1;    //default sign incase if + - is not mentioned
+        while(i<n && s[i]==' ') i++;
+        if(s[i]=='+'||s[i]=='-') {
+            sign = (s[i]=='-')?-1:1;
             i++;
         }
         while(i<n && isdigit(s[i])){
-            result=result*10+(s[i]-'0');//res =4 * 10 + 2 = 42
-            // if(result > INT_MAX){
-            //     while(result > INT_MAX){
-            //         result /=10;
-            //     }
-            // }
-            // else if(sign*result < INT_MIN){
-            //     while(sign*result < INT_MIN){
-            //         result *=10;
-            //     }
-            // }
-            if(sign*result>INT_MAX)return INT_MAX;
-            if(sign*result<INT_MIN)return INT_MIN;
+            answer=answer*10+(s[i]-'0');
+
+            if (sign*answer>INT_MAX) return INT_MAX;
+            if (sign*answer<INT_MIN) return INT_MIN;
 
             i++;
         }
-    return result*sign;
+        return sign*answer;
+        
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int myAtoi(string s) {
+//         long long result=0;
+//         int i=0 ;
+//         int n =s.size();
+//         int sign=1;
+//         while(i<n && s[i]==' ' && s[i]!='-' && s[i] != '+' ){
+//             i++;//i=5
+//         }
+//         if( (s[i]=='-' || s[i]=='+')){
+//             sign= (s[i]=='-')?-1:1;//sign = -1
+//             i++;
+//         }
+//         while(i<n && isdigit(s[i])){
+//             result=result*10+(s[i]-'0');//res =4 * 10 + 2 = 42
+          
+//             if(sign*result>INT_MAX)return INT_MAX;
+//             if(sign*result<INT_MIN)return INT_MIN;
+
+//             i++;
+//         }
+//     return result*sign;
+//     }
+// };
