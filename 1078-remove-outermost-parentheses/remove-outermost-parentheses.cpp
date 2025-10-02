dae7dd04f32d7eed29e2bb169cc321485cba6fd2
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-10-02 23:50:29 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (23.47%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        queue<char> valid ;
+        int right=0 , count=0;
         string answer="";
-        int count=0 , right=0;
         while(right<s.size()){
-            if(s[right]=='(' ){
+            if(s[right]=='('){
                 if(count>0){
-                answer.push_back(s[right]);
+                    answer.push_back(s[right]);
                 }
                 count++;
             }
-            
             if(s[right]==')'){
                 count--;
                 if(count>0){
@@ -23,3 +21,51 @@ public:
         return answer;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//class Solution {
+// public:
+//     string removeOuterParentheses(string s) {
+//         string answer='';
+//         int count=0 , right=0;
+//         while(right<s.size()){
+//             if(s[right]=='(' ){
+//                 if(count>0){
+//                 answer.push_back(s[right]);
+//                 }
+//                 count++;
+//             }
+            
+//             if(s[right]==')'){
+//                 count--;
+//                 if(count>0){
+//                     answer.push_back(s[right]);
+//                 }
+//             }
+//             right++;
+//         }
+//         return answer;
+//     }
+// };
