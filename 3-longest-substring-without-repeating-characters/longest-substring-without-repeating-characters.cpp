6f3fc01b0867171a8179dd78bac0b9f428efa5f5
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-10-04 10:55:47 +0530

    Time: 14 ms (40.08%) | Memory: 14.4 MB (21.76%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -20,25 +20,57 @@
 // };
 
 //OPTIMIZED SOLUTION 
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-        if(s.length()==0){
-            return 0;
-        }
-        int maxans = INT_MIN;
-        int left=0 ; 
-        unordered_set<int> set;
-        for(int right = 0 ; right<s.length() ; right++){
-            if(set.find(s[right])!=set.end()){
-                while(left<right && set.find(s[right])!=set.end()){
-                    set.erase(s[left]);
+    class Solution{
+        public:
+        int lengthOfLongestSubstring(string s){
+            if(s.size()==0) return 0;
+            unordered_set<int>st;
+            int left=0;
+            int maxlen=INT_MIN;
+            for(int right = 0 ; right<s.size() ; right++){
+                while(left<right && st.find(s[right]) != st.end()){
+                    st.erase(s[left]);
                     left++;
                 }
+                st.insert(s[right]);
+                maxlen=max(maxlen , right-left+1);
             }
-            set.insert(s[right]);
-            maxans=max(maxans, right-left+1);
+            return maxlen;
         }
-        return maxans;
-    }
-};
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         if(s.length()==0){
+//             return 0;
+//         }
+//         int maxans = INT_MIN;
+//         int left=0 ; 
+//         unordered_set<int> set;
+//         for(int right = 0 ; right<s.length() ; right++){
+//             if(set.find(s[right])!=set.end()){
+//                 while(left<right && set.find(s[right])!=set.end()){
+//                     set.erase(s[left]);
+//                     left++;
+//                 }
+//             }
+//             set.insert(s[right]);
+//             maxans=max(maxans, right-left+1);
+//         }
+//         return maxans;
+//     }
+// };
