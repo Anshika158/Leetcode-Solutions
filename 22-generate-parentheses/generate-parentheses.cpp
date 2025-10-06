160ef8a4b202cac0be63cc6c4c778b01bcf34a42
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-10-06 10:55:02 +0530

    Time: 5 ms (18.98%) | Memory: 15.6 MB (49.25%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,21 +1,47 @@
+// class Solution {
+// public:
+// //USING RECURSION
+
+//     bool isValid(string c){
+//         int balance=0;
+//         for(char s:c){
+//             if(s=='(') balance++;
+//             else balance-- ;
+//             if(balance<0) return false;
+//         }
+//         return (balance==0);
+//     }
+//     void generateAll(string curr , int n , vector<string>&res){
+//         if(curr.length()==2*n){
+//             if(isValid(curr)){
+//                 res.push_back(curr);
+//                 return;
+//             }
+//         }
+//         generateAll(curr+'(',n,res);
+//         generateAll(curr+')',n,res);
+//     }
+//     vector<string> generateParenthesis(int n) {
+//         //BRUTE FORCE APPROACH
+//         vector<string>res;
+//         generateAll("",n,res);
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
-    void backtrack(int open , int close , int n , string current , vector<string> &result){
-        if(current.size() == 2*n){
-            result.push_back(current);
-            return ;
+    void backtrack(string curr , int open , int close , int n , vector<string>&res){
+        if(curr.size()==2*n){
+            res.push_back(curr);
+            return;
         }
-        if(open < n){
-            backtrack(open + 1 , close , n , current + '(' , result);
-        }
-        if(close<open){
-            backtrack(open  ,close+1 , n , current+')', result);
-        }
+        if(open<n) backtrack(curr+'(' , open+1 , close , n , res);
+        if(close<open) backtrack(curr+')' , open , close+1 , n , res);
     }
-
     vector<string> generateParenthesis(int n) {
-        vector<string> result;
-        backtrack(0, 0, n , "" , result);
-        return result;
+        vector<string>res;
+        backtrack("" , 0 , 0 , n , res);
+        return res;
     }
 };
