77eaf64bfae766959b95f0f6722a7d97fe8bea4f
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-10-08 03:19:46 +0530

    Time: 18 ms (70.74%) | Memory: 15.9 MB (71.73%) - LeetSync

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool hasD(string a , string b){
+        int hashM[26]={0};
+        for(int i=0 ; i<a.size() ; i++){
+            if(hashM[a[i]-'a']>0) return true; //aa bb cc case
+
+            hashM[a[i]-'a']=1;
+        }
+        for(int i=0 ; i<b.size() ; i++){
+            if(hashM[b[i]-'a'] > 0) return true;
+        }
+        int temp[26]={0};
+        for(int i=0 ; i<b.size() ; i++){
+            if(temp[b[i]-'a']>0) return true;
+            temp[b[i]-'a']=1;
+        }
+        return false;
+    }
+    int solve(vector<string>&arr ,int idx , int n, string result ){
+        if(idx==n) return result.length();
+
+        int include=0 ;
+        int exclude=0;
+        if(hasD(result,arr[idx])){
+            exclude=solve(arr,idx+1,n,result);
+        }else{
+            include = solve(arr,idx+1,n,result+arr[idx]);
+
+            exclude = solve(arr,idx+1,n,result);
+        }
+        return max(include , exclude);
+    }
+    int maxLength(vector<string>& arr) {
+        int n=arr.size();
+        string result="";
+         
+         return solve(arr , 0 , n , result);
+    }
+};
