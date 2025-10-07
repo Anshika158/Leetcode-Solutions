471513121c91a1695cda2d10cb9f66b71a185fe0
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-10-08 01:18:55 +0530

    Time: 3 ms (66.36%) | Memory: 8.1 MB (16.23%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
-    int solve(int n , vector<int>&dp){
-        if(n<=1) return n;
-        if(dp[n]!=-1) return dp[n];
+    // int solve(int n , vector<int>&dp){
+    //     if(n<=1) return n;
+    //     if(dp[n]!=-1) return dp[n];
 
-        return dp[n]=solve(n-1 , dp) + solve(n-2 , dp);
-    }
-    int fib(int n) {
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
-    }
+    //     return dp[n]=solve(n-1 , dp) + solve(n-2 , dp);
+    // }
+    // int fib(int n) {
+    //     vector<int>dp(n+1,-1);
+    //     return solve(n,dp);
+    //}
+    // BOTTOM UP APPROACH
+        int fib(int n){
+            if(n<=1) return n;
+            vector<int>store(n+1,-1);
+            store[0]=0;
+            store[1]=1;
+            for(int i=2 ; i<=n ; i++){
+                store[i]=store[i-1]+store[i-2];
+            }
+            return store[n];
+        }
 };
 
 
@@ -17,16 +28,3 @@ public:
 
 
 
-
-
-
-
-// class Solution {
-// public:
-//     int fib(int n){
-//         if(n==0 || n==1){
-//             return n;
-//         }
-//         return fib(n-1)+fib(n-2);
-//     }
-// };
