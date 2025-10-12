ecbcb619bc6a50d66bec56e070d338bda1b86804
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-10-12 23:35:45 +0530

    Time: 1 ms (52.49%) | Memory: 10.2 MB (77.93%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,28 +1,43 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stack<string>answer;
-        string word = "";
-        for(int i=0 ; i<s.size() ; i++){ 
-            if(s[i]!=' '){
-                word +=s[i];
-            }else{
-                if(!word.empty()){
-                    answer.push(word);
-                    word="";
-                }
-            }
-        }
-        if (!word.empty()) {
-            answer.push(word);
-        }
+        // stack<string>answer;
+        // string word = "";
+        // for(int i=0 ; i<s.size() ; i++){ 
+        //     if(s[i]!=' '){
+        //         word +=s[i];
+        //     }else{
+        //         if(!word.empty()){
+        //             answer.push(word);
+        //             word="";
+        //         }
+        //     }
+        // }
+        // if (!word.empty()) {
+        //     answer.push(word);
+        // }
+        // string result="";
+        // while(!answer.empty()){
+        //     result+=answer.top();
+        //     answer.pop();
+        //     if(!answer.empty()){
+        //         result+=" ";
+        //     }
+        // }
+        // return result;
+
+        int left , right =  s.size()-1;
         string result="";
-        while(!answer.empty()){
-            result+=answer.top();
-            answer.pop();
-            if(!answer.empty()){
-                result+=" ";
-            }
+        while(right>=0){
+            while(right>=0 && s[right] == ' ')right--;
+            if(right<0) break ;
+            left=right;
+
+            while(right>=0 && s[right]!=' ') right--;
+                if (!result.empty()) result += " ";
+                result += s.substr(right + 1, left-right);
+            
+            
         }
         return result;
     }
