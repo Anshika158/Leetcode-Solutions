05aeccdcafd7596ae9c9de03efb3b45b437fd368
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-10-14 10:47:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (5.73%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,45 +1,107 @@
+class Solution {
+public:
+    int function(int idx , vector<int>&nums , vector<int>&dp){
+        if(idx>=nums.size()) return 0;
+        if(dp[idx]!=-1) return dp[idx];
+        int pick=nums[idx]+function(idx+2 , nums , dp);
+        int notPick=0+function(idx+1,nums,dp);
+         return dp[idx]=max(pick,notPick);
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n , -1);
+        return function(0 , nums , dp);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // class Solution {
-// public:
-// int function(int ind , vector<int> &nums , vector<int> &dp){
-//     if(ind==0){
-//         return nums[ind];
-//     }
-//     if(ind<0){
-//         return 0;
-//     }
-//     if (dp[ind]!= -1) return dp[ind];
+// // public:
+// // int function(int ind , vector<int> &nums , vector<int> &dp){
+// //     if(ind==0){
+// //         return nums[ind];
+// //     }
+// //     if(ind<0){
+// //         return 0;
+// //     }
+// //     if (dp[ind]!= -1) return dp[ind];
 
-//     int pick = nums[ind]+function(ind-2 , nums , dp);
-//     int notpick = 0 + function(ind-1 , nums , dp);
-//     return dp[ind] = max(pick , notpick);
+// //     int pick = nums[ind]+function(ind-2 , nums , dp);
+// //     int notpick = 0 + function(ind-1 , nums , dp);
+// //     return dp[ind] = max(pick , notpick);
 
-// }
-//     int rob(vector<int>& nums) {
-//         int n = nums.size();
-//         vector<int> dp(n , -1);
-//         return function(n -1, nums , dp);
+// // }
+// //     int rob(vector<int>& nums) {
+// //         int n = nums.size();
+// //         vector<int> dp(n , -1);
+// //         return function(n -1, nums , dp);
         
-//     }
-// };
+// //     }
+// // };
+
+// // SPACE OPTIMIZATION
+
+// // class Solution {
+// // public:
+// // int function(int ind , vector<int> &nums , vector<int> &dp){
+// //     int n = nums.size();
+// //     int prev = nums[0];
+// //     int prev2 = 0;
+
+// //     for(int i = 1; i<n ; i++){
+// //         int take = nums[i];
+// //         if(i>1) take+=prev2;
+
+// //         int notTake = 0 + prev;
+
+// //         int curi = max(take , notTake);
+// //         prev2 = prev;
+// //         prev=curi;
+// //     }
+// //     return prev;
+// // }
+// //     int rob(vector<int>& nums) {
+// //         int n = nums.size();
+// //         vector<int> dp(n , -1);
+// //         return function(n -1, nums , dp);
+        
+// //     }
+// // };
+
+// // TABULATION
 
 // class Solution {
 // public:
 // int function(int ind , vector<int> &nums , vector<int> &dp){
 //     int n = nums.size();
-//     int prev = nums[0];
-//     int prev2 = 0;
+//      dp[0] = nums[0];
 
 //     for(int i = 1; i<n ; i++){
 //         int take = nums[i];
-//         if(i>1) take+=prev2;
+//         if(i>1){
+//              take = take + dp[i-2];
+//         }
+//         int notTake = 0 + dp[i-1];
 
-//         int notTake = 0 + prev;
-
-//         int curi = max(take , notTake);
-//         prev2 = prev;
-//         prev=curi;
+//         dp[i] = max(take , notTake);
+         
 //     }
-//     return prev;
+//     return dp[n-1];
 // }
 //     int rob(vector<int>& nums) {
 //         int n = nums.size();
@@ -48,29 +110,3 @@
         
 //     }
 // };
-
-class Solution {
-public:
-int function(int ind , vector<int> &nums , vector<int> &dp){
-    int n = nums.size();
-     dp[0] = nums[0];
-
-    for(int i = 1; i<n ; i++){
-        int take = nums[i];
-        if(i>1){
-             take = take + dp[i-2];
-        }
-        int notTake = 0 + dp[i-1];
-
-        dp[i] = max(take , notTake);
-         
-    }
-    return dp[n-1];
-}
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n , -1);
-        return function(n -1, nums , dp);
-        
-    }
-};
