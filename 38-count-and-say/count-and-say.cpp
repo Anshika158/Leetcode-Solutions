06b2fe14c8809d540563e3d339fa4be8848e9cf8
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-10-18 22:57:17 +0530

    Time: 7 ms (17.61%) | Memory: 9.9 MB (55.79%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -4,18 +4,40 @@ public:
         if(n==1) return "1";
         string result="";
         string say = countAndSay(n-1);
-        //Processing
+
         for(int i=0 ; i<say.size() ; i++){
             int count=1;
-            char ch = say[i];
-            while(i<say.length()-1  && say[i]==say[i+1] ){
+            char ch=say[i];
+            while(i<say.length() && say[i]==say[i+1]){
                 count++;
                 i++;
             }
-            result+= to_string(count)+string(1,ch);
-
+            result+=to_string(count)+string(1,ch);
         }
         return result;
-
     }
 };
+
+
+
+//class Solution {
+// public:
+//     string countAndSay(int n) {
+//         if(n==1) return "1";
+//         string result="";
+//         string say = countAndSay(n-1);
+//         //Processing
+//         for(int i=0 ; i<say.size() ; i++){
+//             int count=1;
+//             char ch = say[i];
+//             while(i<say.length()-1  && say[i]==say[i+1] ){
+//                 count++;
+//                 i++;
+//             }
+//             result+= to_string(count)+string(1,ch);
+
+//         }
+//         return result;
+
+//     }
+// };
