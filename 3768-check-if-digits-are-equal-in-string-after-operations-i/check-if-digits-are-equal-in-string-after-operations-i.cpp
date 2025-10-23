ee6b03a2dad9f9745e9cfb0ee158bf4031e0e8bc
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-10-24 03:41:19 +0530

    Time: 3 ms (87.25%) | Memory: 9.2 MB (79.47%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-    string check(string r){
-        int n=r.size();
-        string res="";
-        for(int i=1 ; i<n ; i++){
-            int sum = ((r[i-1]-'0')+(r[i]-'0'))%10;
-            res+= to_string(sum);
-        }
-        return res;
-    }
+    //string check(string r){
+    //     int n=r.size();
+    //     string res="";
+    //     for(int i=1 ; i<n ; i++){
+    //         int sum = ((r[i-1]-'0')+(r[i]-'0'))%10;
+    //         res+= to_string(sum);
+    //     }
+    //     return res;
+    // }
     bool hasSameDigits(string s) {
-        string result = s ;
-        while(result.size()>2){
-            result=check(result);
+    //     string result = s ;
+    //     while(result.size()>2){
+    //         result=check(result);
+    //     }
+    //     return result[0]==result[1];
+    int n=s.size();
+    vector<int>digit(n);
+    for(int i=0 ; i<n ; i++){
+        digit[i]=s[i]-'0';
+    }
+    while(n>2){
+        for(int i=0; i<n-1 ; i++){
+            digit[i]=(digit[i]+digit[i+1])%10;
         }
-        // int n=s.size();
-        // for(int i=1 ; i<n ; i++){
-        //     int sum = ((s[i-1]-'0')+(s[i]-'0'))%10;
-        //     result+= to_string(sum);
-        // }
-        return result[0]==result[1];
+        n--;
+    }
+    return digit[0]==digit[1];
     }
 };
