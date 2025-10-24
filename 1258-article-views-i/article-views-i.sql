571d71190886b82612cc8fe58fa8fc42cb76c544
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-10-24 18:39:19 +0530

    Time: 405 ms (53.72%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1258-article-views-i/article-views-i.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+SELECT distinct author_id as id FROM Views WHERE author_id=viewer_id order by id
+
+
+
+
+
+
+
+
+
+
+
+
+# Write your MySQL query statement below
+
+-- select distinct author_id as id from Views
+-- where author_id = viewer_id 
+-- order by id;
