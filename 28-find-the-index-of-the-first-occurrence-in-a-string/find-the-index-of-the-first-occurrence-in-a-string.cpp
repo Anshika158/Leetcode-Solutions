ba80527316e604c8b2bbdbc5f1513941ef966f8c
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-10-26 02:15:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (81.49%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,23 +1,50 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int n=needle.size();
-        int m=haystack.size();
-        if(n==0) return -1;
-        // if(n==1){
-        //     for(int i=0 ; i<m ; i++){
-        //         if(haystack[i]==needle[0]){
-        //             return i;
-        //         }
-        //     }
-        // }
+        int m=haystack.size() , n=needle.size();
+
+        int i=0;
         for(int i=0 ; i<m ; i++){
-           int j=0;
-           while(j<n && haystack[i+j]==needle[j]){
-            j++;
-           }
-           if(j==n) return i;
+            int j=0;
+            while(j<n && haystack[i+j]==needle[j]){ 
+                //i++; as it will not return the first index
+                j++;
+            }
+            if(j==n) return i;
         }
         return -1;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         int n=needle.size();
+//         int m=haystack.size();
+//         if(n==0) return -1;
+//        
+//         for(int i=0 ; i<m ; i++){
+//            int j=0;
+//            while(j<n && haystack[i+j]==needle[j]){
+//             j++;
+//            }
+//            if(j==n) return i;
+//         }
+//         return -1;
+//     }
+// };
