69a4c4d6ce693f04fce5e7aee22df59c241b4382
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-10-25 11:17:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (51.54%) - LeetSync

--- 520-detect-capital/detect-capital.cpp
+++ 520-detect-capital/detect-capital.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        if(all_of(word.begin() , word.end() , :: isupper)){
-            return true;
+        int count=0;
+        for(char &ch : word ){
+            if(isupper(ch)) count++;
         }
-        if(all_of(word.begin() , word.end() , :: islower)){
-            return true;
-        }
-        if(isupper(word[0]) && all_of(word.begin()+1 , word.end() , ::islower )){
-            return true;
-        }
+        if(count==0 || count==word.size() || (count==1 && isupper(word[0]))) return true;
+
         return false;
     }
 };
+//class Solution {
+// public:
+//     bool detectCapitalUse(string word) {
+//         if(all_of(word.begin() , word.end() , :: isupper)){
+//             return true;
+//         }
+//         if(all_of(word.begin() , word.end() , :: islower)){
+//             return true;
+//         }
+//         if(isupper(word[0]) && all_of(word.begin()+1 , word.end() , ::islower )){
+//             return true;
+//         }
+//         return false;
+//     }
+// };
