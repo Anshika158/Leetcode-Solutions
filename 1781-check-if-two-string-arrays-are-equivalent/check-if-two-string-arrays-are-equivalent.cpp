e96c8c812e22826944dd6777491e37d1cb58d0ff
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-10-25 20:07:49 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (28.21%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
@@ -16,4 +16,27 @@ public:
         }
         return true;
     }
-};
+ };
+
+// //same logic using less loop 
+// class Solution{
+//     public:
+//     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2){
+//         int m = word1.size() , n = word2.size();
+//         int w1l=0 , w2l=0 , i=0 , j=0 ;
+//         while(w1l<m && w2l<n){
+//             if(word1[w1l][i]!=word2[w2l][j]) return false;
+//             i++;
+//             j++;
+//             if(i==word1[w1l].size()){
+//                 i=0;
+//                 w1l++;
+//             }
+//             if(j==word2[w2l].size()){
+//                 j=0;
+//                 w2l++;
+//             }
+//         }
+//         return true;
+//     }
+// };
