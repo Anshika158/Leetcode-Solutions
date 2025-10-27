9ee87477ac65860c54feb95e3fcb590012a26c4c
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-10-28 04:24:09 +0530

    Time: 6 ms (62.18%) | Memory: 33 MB (27.80%) - LeetSync

---
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int prev = 0, result = 0;
+        
+        for (string row : bank) {
+            int curr = count(row.begin(), row.end(), '1');
+            if (curr != 0) {
+                result += prev * curr;
+                prev = curr;
+            }
+        }
+        
+        return result;
+    }
+};
