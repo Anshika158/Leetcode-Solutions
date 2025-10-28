2a000a1a63d29b1895724247439a04ddf33afbde
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-10-29 03:25:48 +0530

    Time: 8 ms (40.48%) | Memory: 22.1 MB (68.10%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
@@ -1,38 +1,22 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
-        int n = nums.size();
+        int count = 0, n = nums.size();
+        int sum = 0, halfSum = 0;
 
-        auto canMakeAllZero = [&](int start, int dir) {
-            vector<int> arr = nums;
-            int curr = start;
+        for (int num : nums) sum += num;
 
-            while (curr >= 0 && curr < n) {
-                if (arr[curr] == 0) {
-                    curr += dir;
-                } else {
-                    arr[curr] -= 1;
-                    dir *= -1;
-                    curr += dir;
-                }
-            }
-            // Check if all elements became 0
-            for (int x : arr) {
-                if (x != 0) return false;
-            }
-            return true;
-        };
-
-        int count = 0;
         for (int i = 0; i < n; i++) {
+            halfSum += nums[i];
             if (nums[i] == 0) {
-                if (canMakeAllZero(i, 1)) count++;   // move right
-                if (canMakeAllZero(i, -1)) count++;  // move left
+                if (2 * halfSum == sum) {
+                    count += 2;
+                } else if (abs(sum - 2 * halfSum) == 1) {
+                    count++;
+                } 
             }
         }
+        
         return count;
     }
 };
