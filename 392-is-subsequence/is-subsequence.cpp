a025a0392381028ca9a73941f985ef2cd492a024
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-11-02 05:02:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (66.48%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0 , n1=s.size();
+        int j=0 , n2=t.size();
+
+        while(i<n1 && j<n2){
+            if(s[i]==t[j]){
+                i++;
+            }
+            j++;
+        }
+        return i==s.size();
+    }
+};
