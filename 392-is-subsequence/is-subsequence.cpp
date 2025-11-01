cb228289e081ebfadedcf06a802ed9b21797674a
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-11-02 05:22:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (5.55%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,15 +1,42 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int i=0 , n1=s.size();
-        int j=0 , n2=t.size();
 
-        while(i<n1 && j<n2){
-            if(s[i]==t[j]){
-                i++;
-            }
-            j++;
+//FOR SMALL TEST CASES
+        // int i=0 , n1=s.size();
+        // int j=0 , n2=t.size();
+
+        // while(i<n1 && j<n2){
+        //     if(s[i]==t[j]){
+        //         i++;
+        //     }
+        //     j++;
+        // }
+        // return i==s.size();
+
+// FOR LARGE TESTCASES OR WHERE MULTIPLE S ARE THERE
+        int m=s.size();
+        int n=t.size();
+
+        int prev=-1;
+
+        map<char ,vector<int>>mp;
+
+        for(int i=0; i<n ; i++){
+            mp[t[i]].push_back(i);
         }
-        return i==s.size();
+
+        for(int i=0 ; i<m ; i++){
+            char ch = s[i];
+            if(mp.find(ch)==mp.end()) return false;
+            
+            vector<int>indices(mp[ch]);
+            auto it = upper_bound(begin(indices) ,end(indices) , prev);
+
+            if(it==indices.end()) return false;
+
+            prev=*it;
+        }
+        return true;
     }
 };
