c41e8b429845332311f708238872611ad455a0b1
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-11-05 12:32:09 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (76.88%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-
-    int function(vector<int> &nums ){
-        int n = nums.size();
-        int prev=nums[0];
+    int solve(vector<int>&nums){
+        int n=nums.size();
         int prev2=0;
+        int prev=nums[0];
 
-        for(int i = 1 ; i< n ; i++){
+        for(int i=1 ; i<n ; i++){
             int take = nums[i];
-            if(i>0){
-                take = take + prev2;
+            if(i>1){
+                take = take+prev2;
             }
             int notTake = 0+prev;
-
             int curi = max(take , notTake);
-            prev2 = prev;
-            prev = curi;
+            prev2=prev;
+            prev=curi;
         }
         return prev;
-
     }
-
     int rob(vector<int>& nums) {
-       int n = nums.size();
-       vector<int> temp1 , temp2;
-       if(n==1){
-        return nums[0];
-       }
-       for(int i = 0 ; i<n ; i++){
-        if(i!=0) temp1.push_back(nums[i]);
-        if(i!=n-1) temp2.push_back(nums[i]);
-       }
-       return max(function(temp1) , function(temp2));
+        int n=nums.size();
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[0] ,nums[1]);
+        vector<int>temp1 , temp2;
+        for(int i=0 ; i<n ; i++){
+            if(i!=0)temp1.push_back(nums[i]);
+            if(i!=n-1)temp2.push_back(nums[i]);
+        }
+        return max(solve(temp1) , solve(temp2));
     }
 };
