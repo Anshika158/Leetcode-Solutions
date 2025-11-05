b238f3e3b882230751051184ba2a08d472bc41f1
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-11-05 11:38:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (17.60%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -10,7 +10,7 @@ public:
         dp[1]=1;
         dp[2]=2;
         for(int i=3; i<=n ; i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            dp[i]=dp[i-2]+dp[i-1];
         }
         return dp[n];
     }
