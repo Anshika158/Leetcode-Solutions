fc483bd900c367667b9713e26f5f2335ccd65710
Anshika158 <157527569+Anshika158@users.noreply.github.com>
2025-11-05 11:37:49 +0530

    Time: 2 ms (5.04%) | Memory: 8.6 MB (34.59%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,31 +1,54 @@
 class Solution {
 public:
-//RECURSION + MEMOIZATION
-    int solve(int n , vector<int>&dp){
-        if(n<0){
-            return 0;
-        }
-        if(dp[n]!=-1) return dp[n];
-        if(n==0) return 1;
-        int one_step = solve(n-1 , dp);
-        int two_step = solve(n-2 , dp);
-    return dp[n]=one_step+two_step;
-    }
-    int climbStairs(int n){\
-        vector<int>dp(n+1,-1);
-        return solve(n , dp);
+    // int solve(int n , vector<int>&dp){
+    //     if(n==1 || n==2 ) return dp[n];
+
     // }
-    // int climbStairs(int n) {
-    //     if(n<=2)return n;
-    //     vector<int>dp(n+1);
-    //     dp[1]=1;
-    //     dp[2]=2;
-    //     for(int i=3 ; i<=n ; i++){
-    //         dp[i]=dp[i-2]+dp[i-1];
-    //     }
-    //     return dp[n];
+    int climbStairs(int n) {
+        vector<int>dp(n+1);
+        if(n<=2) return n;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3; i<=n ; i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
+
+
+
+
+//class Solution {
+// public:
+// //RECURSION + MEMOIZATION
+//     int solve(int n , vector<int>&dp){
+//         if(n<0){
+//             return 0;
+//         }
+//         if(dp[n]!=-1) return dp[n];
+//         if(n==0) return 1;
+//         int one_step = solve(n-1 , dp);
+//         int two_step = solve(n-2 , dp);
+//     return dp[n]=one_step+two_step;
+//     }
+//     int climbStairs(int n){
+//         vector<int>dp(n+1,-1);
+//         return solve(n , dp);
+//     // }
+
+
+//     // int climbStairs(int n) {
+//     //     if(n<=2)return n;
+//     //     vector<int>dp(n+1);
+//     //     dp[1]=1;
+//     //     dp[2]=2;
+//     //     for(int i=3 ; i<=n ; i++){
+//     //         dp[i]=dp[i-2]+dp[i-1];
+//     //     }
+//     //     return dp[n];
+//     }
+// };
 
 
 
